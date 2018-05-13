681ca3636c58aa0f030d9ecf209a268b41cffa86
AndjelaMijailovic <AndjelaMijailovicAndja@hotmail.com>
2018-05-14 00:53:16 +0200

    Add files via upload

---
+++ poglavlja/6/kodovi/ChromosomeToCycle.py
@@ -0,0 +1,26 @@
+def chromosome_to_cycle(chromosome):
+
+    nodes = [0 for i in range(2*len(chromosome))]
+    #gde je -1 u knjizi u algoritmu, pisemo -2
+     
+    for j in range(len(chromosome)):
+        i = chromosome[j]
+        if i > 0:
+            #dodajemo cvorove
+            nodes[2*j - 1] = 2*i -1
+            nodes[2*j] = 2*i
+        else:
+            nodes[2*j - 1] = -2*i
+            nodes[2*j] = -2*i -1
+    
+    return nodes
+          
+            
+def main():
+    print(chromosome_to_cycle([1, -2, -3, 4]))
+    
+if __name__ == "__main__":
+    main()
+           
+
+    
---
+++ poglavlja/6/kodovi/CycleToChromosome.py
@@ -0,0 +1,19 @@
+def cycle_to_chromosome(nodes):
+
+    chromosomes = [0 for i in range(len(nodes)//2)]
+    
+    for j in range(len(nodes)//2):
+        if nodes[2*j] < nodes[2*j + 1]:
+            chromosomes[j] = nodes[2*j +1] // 2
+        else:
+            chromosomes[j] = -nodes[2*j] // 2
+    
+    return chromosomes
+    
+def main():
+    print(cycle_to_chromosome([1,2, 4, 3, 5, 6, 7 ,8]))
+    
+if __name__ == "__main__":
+    main()
+            
+
