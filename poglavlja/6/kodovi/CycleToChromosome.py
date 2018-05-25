140d97ff2156260021ab470b72e09e23c7a4575c
AndjelaMijailovic <AndjelaMijailovicAndja@hotmail.com>
2018-05-25 10:45:32 +0200

    Updated

--- poglavlja/6/kodovi/CycleToChromosome.py
+++ poglavlja/6/kodovi/CycleToChromosome.py
@@ -12,8 +12,7 @@ def cycle_to_chromosome(nodes):
     
 def main():
     nodes = [1, 2, 4, 3, 6, 5, 7, 8]
-	
-	print(cycle_to_chromosome(nodes))
+    print(cycle_to_chromosome(nodes))
     
 if __name__ == "__main__":
     main()
